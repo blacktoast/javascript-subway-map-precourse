8d4396dad1db35215de9adf7b39691d6d2682fb1
blacktoast <dbstjd100@naver.com>
2021-11-02 21:17:44 +0900

    feat 구간 역추가 이벤트 구현

--- src/controller/section/section.js
+++ src/controller/section/section.js
@@ -6,12 +6,17 @@ function selectHandler(e) {
   if (e.target.classList.contains("section-line-menu-button")) {
     const line = e.target.dataset.lineId;
     renderSectionInput(line);
-    sectionDeleteEvent(line);
+    sectionDeleteEvent();
+    sectionAddEvent();
   }
 }
 
+function getSectionLine() {
+  return document.querySelector(".section-line").dataset.sectionId;
+}
+
 function checkSectionData() {
-  let lineNum = document.querySelector(".section-line").dataset.sectionId;
+  let lineNum = getSectionLine();
   let line = store.getLines()[lineNum];
   if (line.length < 3) {
     alert("해당 구간의 역의 갯수는 2개이하가 될수없습니다");
@@ -26,6 +31,27 @@ function sectionDeleteHandler(e) {
       console.log(e.target.closest("tr").dataset.sectionOrderId);
     }
   }
+}
+
+function sectionAddHandler(e) {
+  let station = document.querySelector("#section-station-selector").value;
+  let order = $("#section-order-input").value.trim();
+  let line = store.getLines();
+  let lineStations = line[getSectionLine()];
+  lineStations.splice(order, 0, station);
+  store.setLine(line);
+  renderSectionInput(getSectionLine());
+  sectionDeleteEvent();
+  sectionAddEvent();
+  console.log(lineStations, order, line);
+}
+
+function sectionAddEvent() {
+  document
+    .querySelector("#section-add-button")
+    .addEventListener("click", (e) => {
+      sectionAddHandler(e);
+    });
 }
 
 function sectionDeleteEvent() {
